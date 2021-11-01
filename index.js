0f58bcec40763c50206ccb4f34ab957acd16433b
mhshawan <contact.mhshawan@gmail.com>
2021-11-01 21:34:55 +0600

    commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { MongoClient } = require('mongodb');
 const ObjectId = require('mongodb').ObjectId;
-const cors=require('cors');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,57 +11,100 @@ app.use(cors());
 app.use(express.json());
 
 //database
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wvpgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iajji.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 //console.log(uri);
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 //function
-async function run(){
-    try{
+async function run() {
+    try {
         await client.connect();
         console.log('database connected');
-        const database = client.db('onlineBookShop');
-        const bookCollection = database.collection('books');
+        const database = client.db('tour_travels');
+        const tourCollection = database.collection('packages');
         const clientCollection = database.collection('client');
 
-        //console.log(book);
-         //GET product API
-         app.get('/books',async(req,res)=>{
-         // console.log(req.query);
-          const cursor = bookCollection.find({});
-          const books = await cursor.toArray(); 
-          //console.log(books); 
-          res.send(books);
-      });
+        //GET product API
+        app.get('/packages', async (req, res) => {
+            // console.log(req.query);
+            const cursor = tourCollection.find({});
+            const packages = await cursor.toArray();
+            res.send(packages);
+        });
 
-      //use post to get data by keys
-      app.post('books/byKeys',async (req,res)=>{
-          console.log(req.body);
-          const keys = req.body;
-          const query = {key:{$in:keys}};
-          const users = await productCollection.find(query).toArray();
-          res.send(products);
+        // POST API For New Package
+        app.post('/package', async (req, res) => {
+            const newPackage = req.body;
+            const result = await tourCollection.insertOne(newPackage);
+            console.log('Got New Package', req.body);
+            res.json(result);
+        })
 
-      });
+        app.get('/orders', async (req, res) => {
+            const cursor = clientCollection.find({});
+            const orders = await cursor.toArray();
+            res.send(orders)
+        });
 
-      //ADD orders API
-      app.post('/orders', async (req,res)=>{
-          const order = req.body;
-          const result = await clientCollection.insertOne(order);
-          //console.log('order',order);
-          res.json(result);
-      })
+        //ADD orders API
+        app.post('/orders', async (req, res) => {
+            const order = req.body;
+            const result = await clientCollection.insertOne(order);
+            //console.log('order',order);
+            res.json(result);
+        })
+
+        //  Delete API
+        app.delete('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await clientCollection.deleteOne(query);
+            console.log('deleting user with id', id);
+            res.json(result);
+        });
+
+        // PUT API FOR UPDATE
+        app.put('/orders/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const updatedOrder = req.body;
+            console.log(updatedOrder)
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updateDoc = {
+                $set: {
+                    orderStatus: 'Approved',
+                }
+            }
+            const result = await clientCollection.updateOne(filter, updateDoc, options);
+
+            console.log('Update Hitted ', id);
+            res.json(result);
+
+        })
+
+        // GET api for getting orders by USERID
+        app.get('/orders/:uid', async (req, res) => {
+            const USERID = req.params.uid;
+            console.log(USERID)
+            const query = { userID: USERID };
+            const eachUserOrderData = await clientCollection.find(query).toArray();
+            console.log(eachUserOrderData)
+            res.json(eachUserOrderData);
+        });
     }
-    finally{
+
+
+    finally {
         //await client.close();
     }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello bd book delivery shop..');
+    res.send('Hello tour & travels..');
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 })
